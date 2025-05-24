6a66f408bf8d27558aac503802d068f689ebfb12
AflahAmini <aflahamini@gmail.com>
2025-05-24 14:33:38 +0200

    Implement lsh_split_line to parse line into arguments

--- src/main.c
+++ src/main.c
@@ -89,3 +89,38 @@ char *lsh_read_line(void){
 
   return line;
 }
+
+//use whitespace to separate arguments from each other
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  //strtok() return pointers to within the string you give it, and place \0 bytes at the end of each token.
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
